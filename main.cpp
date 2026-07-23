b83ba5e3276002a1d54beb1f4b0b0fe82fb6b7a1
dtatar123 <daniel.tatar123@gmail.com>
2026-07-23 10:27:52 -0400

    it# Please enter the commit message for your changes. Lines starting

--- main.cpp
+++ main.cpp
@@ -30,7 +30,7 @@ void fizzBuzz(int n)
 {
     for (int i = 1; i <= n; i++)
     {
-        if (i % 3 == 0 & i % 5 == 0)
+        if (i % 3 == 0 && i % 5 == 0)
         {
             cout << "fizzBuzz";
         }
